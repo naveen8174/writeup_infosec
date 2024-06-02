b1644cad0b179c37765f6c260273df4270ff40ed
naveen8174 <150158418+naveen8174@users.noreply.github.com>
2024-06-02 08:30:42 +0530

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -1,4 +1,5 @@
 import random
+#giving thee input for no.of chambers in the barrel
 chamber=int(input("enter no.of chambers in the roulette: "))
 
 def load():
@@ -7,12 +8,15 @@ def load():
 def fire():
   die_at=random.randint(1,chamber)
   return die_at
+#assuming the bullet to be always at 1 initially.
 bullet_at=1
 while True:
   print("1.load your gun \n2.fire the gun\nother.exit\n")
   opt=int(input("enter the option: "))
   if opt==1:
+    #(optional) choosing the chamber to place bullet
     bullet_at=load()
+  #fire the gun
   elif opt==2:
     fire_at=fire()
     if fire_at==bullet_at:
